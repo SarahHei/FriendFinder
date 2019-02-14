b6cae25d66ab774ed39c109209a436d8d3a762c4
SarahHei <sarahhei@compasseval.com>
2019-02-14 18:56:04 -0500

    Updates

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+var friendsList = [
+    {
+        "name": "Puggly",
+        "photo": "https://unsplash.com/photos/TPZNooS1Meg",
+        "scores": [
+            4,
+            3,
+            2,
+            4,
+            3,
+            2,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    
+    {
+        "name": "Cricket",
+        "photo": "https://unsplash.com/photos/Q93FCSsmV1M",
+        "scores": [
+            1,
+            5,
+            3,
+            3,
+            5,
+            2,
+            4,
+            2,
+            3,
+            2
+        ]
+    },
+    
+    {
+        "name": "Dusty",
+        "photo": "https://unsplash.com/photos/9lVdutKM9vg",
+        "scores": [
+            2,
+            3,
+            1,
+            5,
+            3,
+            3,
+            1,
+            2,
+            3,
+            5
+        ]
+    },
+    
+    {
+        "name": "Giggles",
+        "photos": "https://unsplash.com/photos/nhFdUcuSmKM",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            4,
+            4,
+            2,
+            2,
+            3,
+            5
+        ]    
+    },
+    
+    {
+        "name": "Simon",
+        "photo": "https://unsplash.com/photos/OuDKs560LcE",
+        "scores": [
+            1,
+            2,
+            5,
+            4,
+            2,
+            2,
+            5,
+            1,
+            2,
+            4
+        ]  
+    }
+];
+
+module.exports = friendsList;
---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsListData = require("../app/data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsListData);
+        
+    });
+    app.post("/api/friends", function (req, res) {
+        friendsListData.push(req.body);
+        res.json(true);
+    })
+    
+}
